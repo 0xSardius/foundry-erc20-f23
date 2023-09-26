2375031803323854f77ec511c3c88e5cb8bd79d7
Sardius <0xsardius@gmail.com>
2023-09-25 20:23:47 -0400

    Manual Token contract

---
+++ src/ManualToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract ManualToken {
+
+    // my address -> 10 tokens
+    mapping(address => uint256) private s_balances;
+
+    // function name() public pure returns (string memory) {
+    //     return "Manual Token";
+    // }
+    string public name = "Manual Token";
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether; // 100 * 10 ** 18
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns(uint256) {
+        return s_balances[_owner];
+    }
+}
